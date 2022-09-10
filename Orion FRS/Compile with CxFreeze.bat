832bcadb9165a759974546a1268cdfa0887b8350
Erick Esau Martinez <90479299+Erickesau@users.noreply.github.com>
2022-09-10 10:55:30 -0600

    Update Compile with CxFreeze.bat

--- Orion FRS/Compile with CxFreeze.bat
+++ Orion FRS/Compile with CxFreeze.bat
@@ -1,2 +1,2 @@
-cxfreeze --base-name=Win32GUI --icon=icon.ico --include-files=media,data_base "Orion FRS v1.2.8.py"
+cxfreeze --base-name=Win32GUI --icon=icon.ico --include-files=media,data_base "Orion FRS v1.2.9.py"
 pause
