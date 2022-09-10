3dc07b33ce845c831360d1f6b279e29a8dc1a894
Erick Esau Martinez <90479299+Erickesau@users.noreply.github.com>
2022-09-10 10:58:46 -0600

    Update compile with pyinstaller.bat

--- Orion FRS/compile with pyinstaller.bat
+++ Orion FRS/compile with pyinstaller.bat
@@ -1,9 +1,9 @@
 color a
 
-pyinstaller --collect-data=face_recognition_models --noconsole --icon=icon.ico "Orion FRS v1.2.8.py"
+pyinstaller --collect-data=face_recognition_models --noconsole --icon=icon.ico "Orion FRS v1.2.9.py"
 
 echo.
 echo copying media folder using xcopy.
-xcopy /I /Y media "dist/Orion FRS v1.2.8/media"
-xcopy /I /Y /S data_base "dist/Orion FRS v1.2.8/data_base"
+xcopy /I /Y media "dist/Orion FRS v1.2.9/media"
+xcopy /I /Y /S data_base "dist/Orion FRS v1.2.9/data_base"
 pause
